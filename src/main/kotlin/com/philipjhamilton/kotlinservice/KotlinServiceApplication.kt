0aefbe6bdb039074f5fc8ca7b700b9fb45e98c16
Philip Hamilton <pjhamilton4@gmail.com>
2020-09-23 00:42:11 -0400

    Adding spring initializer project

---
+++ src/main/kotlin/com/philipjhamilton/kotlinservice/KotlinServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.philipjhamilton.kotlinservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/philipjhamilton/kotlinservice/KotlinServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.philipjhamilton.kotlinservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
